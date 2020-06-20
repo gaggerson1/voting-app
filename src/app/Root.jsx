91816cfd70964c044c97296897b3d07b356e8318
Gerson Almeida <gaggerson1@gmail.com>
2020-06-20 18:28:51 -0500

    Add main structure, Add routes, Link font into project

---
+++ src/app/Root.jsx
@@ -0,0 +1,20 @@
+/**
+ * Libraries
+ */
+import React from 'react';
+
+/**
+ * Dependencies
+ */
+import 'typeface-lato';
+
+/**
+ * Screens
+ */
+import Routes from './Routes';
+
+const Root = () => {
+  return <Routes />;
+};
+
+export default Root;
---
+++ src/app/config/routes.js
@@ -0,0 +1,13 @@
+const ROUTES = {
+  root: '/',
+  home: '/home',
+  signin: '/sign-in',
+  trials: '/trials',
+  howItWorks: '/how-it-works',
+  terms: '/terms',
+  privacy: '/privacy',
+  contact: '/contact',
+  search: '/search',
+};
+
+export default Object.freeze(ROUTES);
